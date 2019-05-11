2424e5126f913528743f7f20bc6f7fd734ddb169
Ryan Waldon <ryanpwaldon@gmail.com>
2019-05-12 01:24:34 +1000

    updated formatNumber filter

--- src/filters/formatNumber.ts
+++ src/filters/formatNumber.ts
@@ -1,18 +1,37 @@
 import Vue from 'vue'
 import * as numeral from 'numeral'
 
+// Vue.filter('formatNumber', (number, format) => {
+//   switch (format) {
+//     case 'price':
+//       switch (true) {
+//         case number > 1000000:
+//           return numeral(number).format('$0.00a')
+//         case number > 100000:
+//           return numeral(number).format('$0a')
+//         case number > 1000:
+//           return numeral(number).format('$0.00a')
+//         default:
+//           return numeral(number).format('$0')
+//       }
+//     case 'percentage':
+//       return numeral(number).format('%0.00')
+//     default: return number
+//   }
+// })
+
 Vue.filter('formatNumber', (number, format) => {
   switch (format) {
     case 'price':
       switch (true) {
         case number > 1000000:
-          return numeral(number).format('$0.00a')
+          return numeral(number).format('$0,0')
         case number > 100000:
-          return numeral(number).format('$0a')
+          return numeral(number).format('$0,0')
         case number > 1000:
-          return numeral(number).format('$0.00a')
+          return numeral(number).format('$0,0')
         default:
-          return numeral(number).format('$0')
+          return numeral(number).format('$0,0')
       }
     case 'percentage':
       return numeral(number).format('%0.00')
