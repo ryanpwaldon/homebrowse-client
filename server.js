a899807a492ae50037cf006edc7dd84594731eea
Ryan Waldon <ryanpwaldon@gmail.com>
2019-07-20 19:49:04 +1000

    updates

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 app.use(express.static(__dirname + '/dist'))
-app.all('*', (_, res) => res.sendFile(path.join(__dirname + './dist/index.html')))
+app.get('*', (_, res) => res.sendFile(path.join(__dirname + './dist/index.html')))
 app.listen(port, () => console.log(`Listening on ${port}.`))
